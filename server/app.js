dc7dcdd9c9be0348a1cd3cf3eea395df97bc0fb2
John Tan <tantai.john@gmail.com>
2014-09-07 21:22:45 -0500

    Removed more socket.io

--- server/app.js
+++ server/app.js
@@ -19,8 +19,6 @@ mongoose.connect(config.mongo.uri, config.mongo.options);
 // Setup server
 var app = express();
 var server = require('http').createServer(app);
-// var socketio = require('socket.io').listen(server);
-// require('./config/socketio')(socketio);
 require('./config/express')(app);
 require('./routes')(app);
 
